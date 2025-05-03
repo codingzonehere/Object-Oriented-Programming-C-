03cf2eea8c3033ed90df1abfc726db974f86d04a
Tanvir Ahmed (Biplob) <75024294+codingzonehere@users.noreply.github.com>
2025-05-03 22:02:32 +0600

    Array_of_objects.cpp

--- Array Of Object/Array_of_objects.cpp
+++ Array Of Object/Array_of_objects.cpp
@@ -1,5 +1,28 @@
-#include<bits/stdc++.h>
+/****************************************************************************************************
+***************************************AUTHOR: TANVIR AHMED******************************************
+*********************************************CSE, DUET***********************************************
+****************************************************************************************************/
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <vector>
+#define testcase int t; cin>>t; while(t--)
+#define iosbse ios_base::sync_with_stdio(false)
+#define cintie cin.tie(NULL)
+#define ll long long int
+#define forloop for (int i = 0; i < n; ++i)
+#define vecti vector<int>
+#define vect vector<ll>
+#define sr string
+#define YS cout << "YES" <<endl
+#define NO cout << "NO" <<endl
+#define pb(x) push_back(x)
+#define sz(x) trunc(log10(x)) + 1
+#define setpre(x) cout<<fixed<<setprecision(x)<<endl;
+
 using namespace std;
+
 class Person{
     public:
     void display(int i){
