13ce1f5e1a911d1d222ba129fbe5fa10c1775e50
Tanvir Ahmed (Biplob) <75024294+codingzonehere@users.noreply.github.com>
2025-05-03 22:04:05 +0600

    Exception_Handling[sqrt-ve].cpp

--- Exception Handling/Exception_Handling[sqrt-ve].cpp
+++ Exception Handling/Exception_Handling[sqrt-ve].cpp
@@ -5,8 +5,31 @@
 // catch( ExceptionName e1 )  {   
 //      // catch block catches the exception that is thrown from try block
 // } 
+/****************************************************************************************************
+***************************************AUTHOR: TANVIR AHMED******************************************
+*********************************************CSE, DUET***********************************************
+****************************************************************************************************/
 #include <bits/stdc++.h>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <vector>
+#define testcase int t; cin>>t; while(t--)
+#define iosbse ios_base::sync_with_stdio(false)
+#define cintie cin.tie(NULL)
+#define ll long long int
+#define forloop for (int i = 0; i < n; ++i)
+#define vecti vector<int>
+#define vect vector<ll>
+#define sr string
+#define YS cout << "YES" <<endl
+#define NO cout << "NO" <<endl
+#define pb(x) push_back(x)
+#define sz(x) trunc(log10(x)) + 1
+#define setpre(x) cout<<fixed<<setprecision(x)<<endl;
+
 using namespace std;
+
 double my_sqrt(int n)
 {
     if(n<0) throw 0;
